5da8126aa5f175f638167bd0a1a094705394165a
Philipp <41761223+philipp2310@users.noreply.github.com>
2021-01-24 01:41:57 +0100

    adjusted widget for new way

--- widgets/BringShoppingList.py
+++ widgets/BringShoppingList.py
@@ -27,9 +27,9 @@ class BringShoppingList(Widget):
 			details = bringList.get_items_detail()
 			itemList = [{"text": self.translate(item['name'], translation), "image": self.get_image(details, item['name'])} for item in items]
 
-			return json.dumps(itemList)
+			return itemList
 		except Exception as e:
-			return json.dumps({'success':False,'error':str(e)})
+			return {'success':False,'error':str(e)}
 
 
 	@staticmethod
--- widgets/js/BringShoppingList.js
+++ widgets/js/BringShoppingList.js
@@ -1,12 +1,20 @@
-(function () {
+class BringShoppingList_BringShoppingList {
 
-	function getIconName(val) {
+	constructor(uid, widgetId) {
+		this.uid = uid;
+		this.widgetId = widgetId;
+		this.refresh();
+		self = this
+		setInterval(function() { self.refresh() }, 1000* 10);
+	}
+
+	static getIconName(val) {
 		let fileName = val.toLowerCase();
 		let repl = {
-			"\\s": "_",
-			" ": "_",
-			"-": "_",
-			"!": "",
+			"\\s" : "_",
+			" "   : "_",
+			"-"   : "_",
+			"!"   : "",
 			"\xe4": "ae",
 			"\xf6": "oe",
 			"\xfc": "ue",
@@ -19,25 +27,27 @@
 		return fileName + '.png'
 	}
 
-
-	function refresh() {
-		$.ajax({
-			url: '/home/widget/',
-			data: JSON.stringify({
-				skill: 'BringShoppingList',
-				widget: 'BringShoppingList',
-				func: 'getList',
-				param: ''
-			}),
-			contentType: 'application/json',
-			dataType: 'json',
-			type: 'POST'
-		}).done(function (answer) {
+	refresh() {
+		self = this
+		fetch(`http://${window.location.hostname}:5001/api/v1.0.1/widgets/${this.widgetId}/function/getList/`, {
+			method: 'POST',
+			body: '{}',
+			headers: {
+				'auth': localStorage.getItem('apiToken'),
+				'content-type': 'application/json'
+			}
+		}).then(function(answer){
 			let $list = $('#BringShoppingList_list');
-			if ('success' in answer){
+			if (!answer.ok) {
+				$list.html("<div class='BringWidgetError'>Bring! Shopping List - ERROR: " + answer.statusText + "</div>");
+			} else if ('success' in answer && !answer['success']) {
 				$list.html("<div class='BringWidgetError'>Bring! Shopping List - ERROR: " + answer['error'] + "</div>");
 				return;
 			}
+			return answer.json()
+		}).then(function (answer) {
+			let $list = $('#BringShoppingList_list');
+			answer = answer.data
 
 			// Build a list of items that are on the list
 			let items = {};
@@ -46,7 +56,7 @@
 			});
 
 			// First remove what's gone from the list
-			$list.find('img').each(function() {
+			$list.find('img').each(function () {
 				if (!($(this).attr('id') in items)) {
 					$(this).parent().remove();
 				}
@@ -54,24 +64,19 @@
 
 			// Then add what's new
 			for (const [itemName, item] of Object.entries(items)) {
-				if ($(`#${itemName}`).length === 0) {
+				if(item['image'] === undefined) return;
+				if ($(`#${BringShoppingList_BringShoppingList.getIconName(item['image'])}`).length === 0) {
 					$list.append(
 						$(`
 							<div class="tile">
 								<div class="BringShoppingTile_imgCont"></div>
-								<img alt="${itemName}" class="BringShoppingItemIcon" id="${itemName}" src="https://web.getbring.com/assets/images/items/${getIconName(item['image'])}" 
+								<img alt="${itemName}" class="BringShoppingItemIcon" id="${BringShoppingList_BringShoppingList.getIconName(item['image'])}" src="https://web.getbring.com/assets/images/items/${BringShoppingList_BringShoppingList.getIconName(item['image'])}" 
 								onerror="this.onerror=null; this.src='https://web.getbring.com/assets/images/items/${item['image'][0].toLowerCase()}.png';"/>
 								<br/>${itemName}</div>
 						`)
 					);
 				}
 			}
-		});
+		})
 	}
-
-	refresh();
-	setInterval(function () {
-		refresh()
-	}, 1000 * 10);
-
-})();
+}
